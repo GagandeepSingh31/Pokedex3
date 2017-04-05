8ba14115e64921456995938368c0faf824003d29
Gagandeeps-Air <you@example.com>
2017-04-05 14:18:51 +0530

    Updated all the IBOutlet with alamofire

---
+++ Pokedex3/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  Pokedex3
+//
+//  Created by Gagandeep Singh on 4/4/17.
+//  Copyright © 2017 Gagandeep Singh. All rights reserved.
+//
+
+import Foundation
+
+
+let URL_BASE = "https://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
+
--- Pokedex3/PokemonDetailVC.swift
+++ Pokedex3/PokemonDetailVC.swift
@@ -8,17 +8,69 @@
 
 import UIKit
 
+
 class PokemonDetailVC: UIViewController {
 
     
+    @IBOutlet weak var attackLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var defenceLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var mainImg: UIImageView!
     @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    @IBOutlet weak var evoLbl: UILabel!
+    @IBOutlet weak var pokedexLbl : UILabel!
+    
     var pokemon : Pokemon!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         nameLbl.text = pokemon.name.capitalized
-
+        let img = UIImage(named: "\(pokemon.pokedexId)")
+        mainImg.image = img
+        currentEvoImg.image = img
+        pokedexLbl.text = "\(pokemon.pokedexId)"
+        
+        
+        
+        
+        
+        pokemon.downloadPokemonDetails { 
+            //Whatever we write will only be called after the network call is completed.
+            
+            
+            print("Did arrive here")
+            self.udpateUI()
+            
+        }
+        
+    }
+    
+    
+    func udpateUI() {
+        
+        attackLbl.text = pokemon.attack
+        defenceLbl.text = pokemon.defense
+        heightLbl.text = pokemon.height
+        weightLbl.text = pokemon.weight
+        typeLbl.text = pokemon.type
+        descriptionLbl.text = pokemon.description
+        
+        if pokemon.nextEvolutionId == "" {
+            evoLbl.text = "No Evolutions"
+            nextEvoImg.isHidden = true
+        }else {
+            nextEvoImg.isHidden = false
+            nextEvoImg.image = UIImage(named: pokemon.nextEvolutionId)
+            let str = "Next Evolution : \(pokemon.nextEvolutionName) - LVL \(pokemon.nextEvolutionLevel)"
+            evoLbl.text = str
+        }
+        
         
         
     }
